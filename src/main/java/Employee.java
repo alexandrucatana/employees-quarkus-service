b6fe552b51e43b81df767c3a3cb6c8d495627e89
alexandrucatana <alexandrucatana81@gmail.com>
2023-03-28 20:43:22 +0200

    first commit to employees quarkus service, tests need to be added

---
+++ src/main/java/Employee.java
@@ -0,0 +1,34 @@
+package org.acme.hibernate.orm.panache;
+
+import io.quarkus.hibernate.reactive.panache.PanacheEntity;
+import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
+
+import javax.persistence.*;
+
+@Entity
+@Cacheable
+@Table(name = "employees")
+public class Employee extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column
+    public String first_name;
+    @Column
+    public String last_name;
+    @Column
+    public String email;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, String email) {
+        this.first_name = firstName;
+        this.last_name = lastName;
+        this.email = email;
+    }
+
+
+}
---
+++ src/main/java/EmployeeResource.java
@@ -0,0 +1,143 @@
+package org.acme.hibernate.orm.panache;
+
+import static javax.ws.rs.core.Response.Status.CREATED;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+import static javax.ws.rs.core.Response.Status.NO_CONTENT;
+
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.jboss.logging.Logger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import io.quarkus.hibernate.reactive.panache.Panache;
+import io.quarkus.panache.common.Sort;
+import io.smallrye.mutiny.CompositeException;
+import io.smallrye.mutiny.Uni;
+
+@Path("employees")
+@ApplicationScoped
+@Produces("application/json")
+@Consumes("application/json")
+public class EmployeeResource {
+
+    private static final Logger LOGGER = Logger.getLogger(EmployeeResource.class.getName());
+
+    @GET
+    public Uni<List<Employee>> get() {
+        return Employee.listAll(Sort.by("last_name"));
+    }
+
+    @GET
+    @Path("{id}")
+    public Uni<Employee> getSingle(Long id) {
+        return Employee.findById(id);
+    }
+
+    @POST
+    @Path("/create")
+    public Uni<Response> create(Employee Employee) {
+        if (Employee == null || Employee.id != null) {
+            throw new WebApplicationException("Id was invalidly set on request.", 422);
+        }
+
+        return Panache.withTransaction(Employee::persist)
+                .replaceWith(Response.ok(Employee).status(CREATED)::build);
+    }
+
+    @PUT
+    @Path("{id}")
+    public Uni<Response> update(Long id, Employee updateEmployee) {
+        if (updateEmployee == null || updateEmployee.first_name == null) {
+            throw new WebApplicationException("Employee name was not set on request.", 422);
+        }
+
+        return Panache
+                .withTransaction(() -> Employee.<Employee> findById(id)
+                        .onItem().ifNotNull().invoke(entity ->
+                                entity.first_name = updateEmployee.first_name
+                        )
+                )
+                .onItem().ifNotNull().transform(entity -> Response.ok(entity).build())
+                .onItem().ifNull().continueWith(Response.ok().status(NOT_FOUND)::build);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Uni<Response> delete(Long id) {
+        return Panache.withTransaction(() -> Employee.deleteById(id))
+                .map(deleted -> deleted
+                        ? Response.ok().status(NO_CONTENT).build()
+                        : Response.ok().status(NOT_FOUND).build());
+    }
+
+    /**
+     * Create a HTTP response from an exception.
+     *
+     * Response Example:
+     *
+     * <pre>
+     * HTTP/1.1 422 Unprocessable Entity
+     * Content-Length: 111
+     * Content-Type: application/json
+     *
+     * {
+     *     "code": 422,
+     *     "error": "Employee name was not set on request.",
+     *     "exceptionType": "javax.ws.rs.WebApplicationException"
+     * }
+     * </pre>
+     */
+    @Provider
+    public static class ErrorMapper implements ExceptionMapper<Exception> {
+
+        @Inject
+        ObjectMapper objectMapper;
+
+        @Override
+        public Response toResponse(Exception exception) {
+            LOGGER.error("Failed to handle request", exception);
+
+            Throwable throwable = exception;
+
+            int code = 500;
+            if (throwable instanceof WebApplicationException) {
+                code = ((WebApplicationException) exception).getResponse().getStatus();
+            }
+
+            // This is a Mutiny exception and it happens, for example, when we try to insert a new
+            // Employee but the name is already in the database
+            if (throwable instanceof CompositeException) {
+                throwable = ((CompositeException) throwable).getCause();
+            }
+
+            ObjectNode exceptionJson = objectMapper.createObjectNode();
+            exceptionJson.put("exceptionType", throwable.getClass().getName());
+            exceptionJson.put("code", code);
+
+            if (exception.getMessage() != null) {
+                exceptionJson.put("error", throwable.getMessage());
+            }
+
+            return Response.status(code)
+                    .entity(exceptionJson)
+                    .build();
+        }
+
+    }
+}
